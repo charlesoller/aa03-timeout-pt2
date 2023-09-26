708fa7b8f9674572c700ea9d95e3376c85b3589c
charlesoller <charlesrello@gmail.com>
2023-09-26 19:08:00 -0400

    Finished All

--- problems/05-postpone.js
+++ problems/05-postpone.js
@@ -29,15 +29,15 @@ function postpone(cb, delay) {
   }
 }
 
-const sayHello = () => console.log('hi');
-const slowHello = postpone(sayHello, 1000);
-console.log(slowHello); // [Function]
-slowHello(); // prints 'hi' after 1000 ms
+// const sayHello = () => console.log('hi');
+// const slowHello = postpone(sayHello, 1000);
+// console.log(slowHello); // [Function]
+// slowHello(); // prints 'hi' after 1000 ms
 
-const sayGoodbye = () => console.log('bye');
-const slowerGoodbye = postpone(sayGoodbye, 1750);
-console.log(slowerGoodbye); // [Function]
-slowerGoodbye(); // prints 'bye' after 1750 ms
+// const sayGoodbye = () => console.log('bye');
+// const slowerGoodbye = postpone(sayGoodbye, 1750);
+// console.log(slowerGoodbye); // [Function]
+// slowerGoodbye(); // prints 'bye' after 1750 ms
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = postpone;
