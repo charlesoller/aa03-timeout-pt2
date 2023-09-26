0241b8490a9ef327016ac81e4b6bbfe777b84c56
Christian E <christianelena92@gmail.com>
2023-09-26 17:46:09 -0500

    solve dynaimc interval count async

--- problems/06-dynamic-interval-count.js
+++ problems/06-dynamic-interval-count.js
@@ -23,21 +23,28 @@ const timeoutObject = dynamicIntervalCount(function() {
 
 console.log(timeoutObject); // Timeout { ... }
 ***********************************************************************/
-
+// input: 
+// output: 
 function dynamicIntervalCount(cb, delay, amount) {
-  // Your code here
+  return intervalObj = setInterval(() => {
+    amount--; 
+    if (amount === 0) {
+      clearInterval(intervalObj); 
+    }
+    cb(); 
+  }, delay);  
 }
 
-dynamicIntervalCount(function() {
-  console.log('hi');
-}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+// dynamicIntervalCount(function() {
+//   console.log('hi');
+// }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 
+// const timeoutObject = dynamicIntervalCount(function() {
+//   console.log('hi');
+// }, 500); // prints 'hi' at 500ms intervals indefinitely
 
-const timeoutObject = dynamicIntervalCount(function() {
-  console.log('hi');
-}, 500); // prints 'hi' at 500ms intervals indefinitely
+// console.log(timeoutObject); // Timeout { ... }
 
-console.log(timeoutObject); // Timeout { ... }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = dynamicIntervalCount;
